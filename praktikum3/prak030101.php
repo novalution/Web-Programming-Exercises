16251f64227d1ece273d7fa677fadb0650cfb4ad
novalution <57666913+novalution@users.noreply.github.com>
2021-04-05 14:03:58 +0700

    Add files via upload

---
+++ praktikum3/prak030101.php
@@ -0,0 +1,9 @@
+<?php
+function gandakanString($s,$n) {
+    for ($x = 0; $x < $n; $x++) {
+        echo "Hello";
+}
+}
+
+gandakanString("Hello",3); // call the function
+?>
---
+++ praktikum3/prak030201.php
@@ -0,0 +1,21 @@
+<?php
+
+function hitungGaji($gol, $masaKerja){
+ 	if ($gol == "A"){
+  		if ($masaKerja < 10) {
+ 			$gaji = 5000000;
+  		} else {
+  			$gaji = 7000000;
+  		}
+ 	} else if ($gol == "B"){
+ 			if ($masaKerja < 10) {
+ 				$gaji = 6000000;
+  			} else {
+  				$gaji = 8000000;
+  			}
+  		}
+return $gaji;
+}
+
+echo hitungGaji("A",8);
+?>
