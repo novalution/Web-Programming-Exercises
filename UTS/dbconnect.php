f723ed3cf1f3e8315b3589b06ec91f5ed241f4bd
novalution <57666913+novalution@users.noreply.github.com>
2021-05-08 13:06:22 +0700

    Delete UTS directory

--- UTS/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $dbhost = "localhost";
-    $dbuser = "id11461439_root";
-    $dbpass = "</Wdy5>M0>LxM8PL";
-    $dbname = "id11461439_game";
-
-$conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
-$conn -> close();
-?>
--- UTS/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-setcookie('nama', $_COOKIE['nama'], time()- 3600,'/');
-setcookie('email', $_COOKIE['email'], time()- 3600,'/');
-header('location:index.php');
-?>
